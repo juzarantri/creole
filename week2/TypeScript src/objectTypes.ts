1b7579ec9e8a0dcde5cbfbba48b35de304085980
Juzar Antri <juzarantri786@gmail.com>
2023-01-30 12:30:45 +0530

    tyescript objects done

---
+++ week2/TypeScript src/objectTypes.ts
@@ -0,0 +1,7 @@
+const person : { name : string, phoneNo: number, isMarried : boolean } = {
+    name : "Juzar",
+    phoneNo : 7846546313,
+    isMarried : false
+};
+
+console.log(person.name);
---
+++ week2/build/objectTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+const person = {
+    name: "Juzar",
+    phoneNo: 7846546313,
+    isMarried: false
+};
+console.log(person.name);
