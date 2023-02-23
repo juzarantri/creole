dd72911479204f3aafb8211bb4b561f5d3cd6935
Juzar Antri <juzarantri786@gmail.com>
2023-02-23 16:38:54 +0530

    Created boilerplate with express,nodemon,@types/node package

--- week5/microservice example/server.js
+++ week5/microservice example/server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3000;
 //// mapping the routes
 const routes = require("./api/routes");
 routes(app);
+
 //// starting a server
 app.listen(port, () => {
   console.log("Server started on port: ", +port);
 });
+  
