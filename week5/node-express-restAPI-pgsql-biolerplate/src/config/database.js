f3c28e0ef9e0d301ad3ef53931b1a1d1e618e65d
Juzar Antri <juzarantri786@gmail.com>
2023-02-27 10:38:32 +0530

    package.json week5

--- week5/node-express-restAPI-pgsql-biolerplate/src/config/database.js
+++ week5/node-express-restAPI-pgsql-biolerplate/src/config/database.js
@@ -1,13 +1,26 @@
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 /// creating connection pool
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 pool.on("connect", () => {
   console.log("Database connected successfully....");
 });
+
+async function test() {
+  const now = await pool.query("Select now();");
+  return now;
+}
+
+//// IIFE (Immediately Invoked Function Expression)
+(async () => {
+  const value = await test();
+  console.log(value.rows[0]["now"]);
+})();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
