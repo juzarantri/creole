a8f05bb5268a91d97d42bad298670f167092ad63
Juzar Antri <juzarantri786@gmail.com>
2023-02-27 14:42:51 +0530

    used fs http and url inbuilt modules

---
+++ week6/nodejs tutorial/http_url_fs/demo_fileserver.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    let fileName = "." + q.pathname;
+
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 not found....");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
