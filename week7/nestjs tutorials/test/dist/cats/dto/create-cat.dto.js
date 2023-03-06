55ac60ca465ddc94d98f8feb757090f3b8e15cfe
iamjuzarantri <juzarantri786@gmail.com>
2023-03-06 16:29:52 +0530

    crontrollers and providers done in nest js

---
+++ week7/nestjs tutorials/test/dist/cats/dto/create-cat.dto.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateCatDto = void 0;
+var CreateCatDto = /** @class */ (function () {
+    function CreateCatDto() {
+    }
+    return CreateCatDto;
+}());
+exports.CreateCatDto = CreateCatDto;
